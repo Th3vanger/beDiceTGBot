1aaf8e165d131c633bb955325db5e38cc3e67c63
Giuseppe Gatto <giuseppe.gatto@caffeina.com>
2021-10-05 17:40:22 +0200

    Added my project

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Composer } = require('micro-bot');
+
+const bot = new Composer()
+//const bot = new Telegraf('2090668934:AAGyXIOia3YiNQGhKzRM2ZPIqpBmdX2oiao');
+//method for invoking start command
+ 
+bot.command('start', ctx => {
+    console.log(ctx.from)
+    bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot. ' + ctx.chat.id, {
+    })
+})
+
+
+//method to start get the script to pulling updates for telegram 
+
+//bot.launch();
+module.exports = bot
