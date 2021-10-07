d0d8f1e4fe77602e8f32a330bfcd4a4a3b69e6af
Giuseppe Gatto <giuseppe.gatto@caffeina.com>
2021-10-07 17:04:04 +0200

    push

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const { Composer } = require('micro-bot');
-
+const fs = require('fs')
 const bot = new Composer()
-//const bot = new Telegraf('2090668934:AAGyXIOia3YiNQGhKzRM2ZPIqpBmdX2oiao');
+
 //method for invoking start command
  
 bot.command('start', ctx => {
     console.log(ctx.from)
-    ctx.reply('hello there! Welcome to my new telegram bot. ' + ctx.chat.id)
+    
+    const https = require('https');
+
+    https.get('https://cf-dice.herokuapp.com/dice/airc/roll', (resp) => {
+    let data = '';
+
+    // A chunk of data has been received.
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        console.log(JSON.parse(data).explanation);
+        console.log(data);
+        ctx.replyWithPhoto({ url: 'https://cf-dice.herokuapp.com/uploads/small_Heavy_b93fa6f364.jpeg' })
+        //ctx.reply(JSON.parse(data));
+    });
+  
+    }).on("error", (err) => {
+        ctx.reply(' unlucky vez ');
+  console.log("Error: " + err.message);
+    });
+    
 })
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
